9b98cfaab1cc6b6bc8e2716858c6f2274988fbad
adembogatekin <pektekin99@gmail.com>
2023-04-12 23:39:02 +0300

    12.04.2023

--- src/5-Lists/Lists.js
+++ src/5-Lists/Lists.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Garage from "./Garage"
+
 const Lists = () => {
   return (
     <div>
-      <Garage/>
+        <Garage/>
     </div>
   )
 }
 
-export default Lists 
+export default Lists
