513f4c17e1d9f5fed20beee8e7efa1607913dd14
adembogatekin <pektekin99@gmail.com>
2023-04-15 11:26:49 +0300

    15.04.2023

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import './App.css';
+
+
 // import JSX from "./1-JSX/JSX"
 // import Props from "./2-Props/Props"
-// import Events from "./3-Events/Events",
+// import Events from "./3-Events/Events"
 // import Conditionals from "./4-Conditionals/Conditionals"
-import Lists from "./5-Lists/Lists"
+// import Lists from "./5-Lists/Lists"
+// import UseStateHook from './6-UseStateHook/UseStateHook';
+// import UseEffectHook from './7-UseEffectHook/UseEffectHook';
+// import Forms from './8-Forms/Forms';
+// import Router from './9-Router/Router';
+// import UseContext from './10-UseContextHook/UseContext';
+import CSSStyling from './11-CSSStyling/CSSStyling';
+
+import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      {/* <JSX/> */}
-      {/* <Props/> */}
-      {/* <Events/> */}
-      {/* {<Conditionals/>} */}
-      <Lists/>
+     {/* <JSX/> */}
+     {/* <Props/> */}
+     {/* <Events/> */}
+    {/* <Conditionals/> */}
+    {/* <Lists/> */}
+    {/* <UseStateHook /> */}
+    {/* <UseEffectHook/> */}
+    {/* <Forms /> */}
+    {/* <Router/> */}
+    {/* <UseContext/> */}
+    <CSSStyling/>
     </div>
   );
 }
